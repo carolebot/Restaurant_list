3253a2dad67c24b456ee243677666d9803356d9b
carolebot <carol.tang79@gmail.com>
2021-08-05 13:35:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Restaurant List
+![./demo.png]
+
+## Features
+- You can see 8 restaurants information in this website (photo, name, category, rating)
+- You can click each restaurant to see more information (phone, address...)
+- You can use search bar to find restaurant (type name or category)
+
+## What I use 
+- express: 4.17.1, express-handlebars: 5.3.2
+- Bootstrap, jQuery, Popper.js and Font Awesome
+
+## How to download
+- git clone https://github.com/carolebot/A1_restaurant_list.git
---
+++ app.js
@@ -0,0 +1,51 @@
+// set express
+const express = require('express')
+const app = express()
+
+// set port 
+const port = 3000
+
+// set tools
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+
+
+// template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// set static file
+app.use(express.static('public'))
+
+
+
+// set route
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  // console.log(req.params.restaurant_id)
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  if (!restaurants.length) {
+    return res.render('noresult', { restaurants: restaurants, keyword: keyword })
+  }
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+
+
+// server start and listen
+app.listen(port, () => {
+  console.log(`running https://localhost:${port}`)
+})
